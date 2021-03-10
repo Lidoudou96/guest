46663206cb29df5d545cb7537490f3aeb37099d6
Lidoudou96 <lidoudou_sz@163.com>
2021-03-10 18:53:57 +0800

    8.3.2完成未验证

--- guest/sign/tests.py
+++ guest/sign/tests.py
@@ -3,31 +3,31 @@ from sign.models import Event,Guest
 from django.contrib.auth.models import User
 
 # Create your tests here.
-class ModelTest(TestCase):
-
-    def setUp(self):
-        Event.objects.create(id=1,name="oneplus 3 event",status=True,limit=2000,address='shenzhen',start_time='2016-08-31 02:18:22')
-        Guest.objects.create(id=1,event_id=1,realname='alen',phone='13711001101',email='alen@mail.com',sign=False)
-
-    def test_event_models(self):
-        result = Event.objects.get(name="oneplus 3 event")
-        self.assertEqual(result.address,"beijing")
-        self.assertTrue(result.status)
-
-    def test_guest_models(self):
-        result = Guest.objects.get(phone='13711001101')
-        self.assertEqual(result.realname, "alen")
-        self.assertFalse(result.sign)
-
-class IndexPageTest(TestCase):
-#测试index登录首页
-    def test_index_page_renders_index_template(self):
-        #测试index视图
-        response = self.client.get('/index')
-        self.assertEqual(response.status_code,200)
-        self.assertTemplateUsed(response,'index.html')
-
-
+# class ModelTest(TestCase):
+#
+#     def setUp(self):
+#         Event.objects.create(id=1,name="oneplus 3 event",status=True,limit=2000,address='beijing',start_time='2016-08-31 02:18:22')
+#         Guest.objects.create(id=1,event_id=1,realname='alen',phone='13711001101',email='alen@mail.com',sign=False)
+#
+#     def test_event_models(self):
+#         result = Event.objects.get(name="oneplus 3 event")
+#         self.assertEqual(result.address,"beijing")
+#         self.assertTrue(result.status)
+#
+#     def test_guest_models(self):
+#         result = Guest.objects.get(phone='13711001101')
+#         self.assertEqual(result.realname, "alen")
+#         self.assertFalse(result.sign)
+#
+# class IndexPageTest(TestCase):
+# #测试index登录首页
+#     def test_index_page_renders_index_template(self):
+#         #测试index视图
+#         response = self.client.get('/index')
+#         self.assertEqual(response.status_code,200)
+#         self.assertTemplateUsed(response,'index.html')
+#
+#
 class LoginActionTest(TestCase):
     #测试登录动作
     def setUp(self):
@@ -63,13 +63,23 @@ class EventManageTest(TestCase):
     #测试发布会管理
 
     def setUp(self):
-        User.objects.create_user('admin','admin@mail.com','admin123456')
+        User.objects.create_user('admin','admin1@mail.com','admin123456')
+
+    def test_add_admin(self):
+        # 测试添加用户
+        user = User.objects.get(username='admin')
+        self.assertEqual(user.username, "admin")
+        self.assertEqual(user.email, "admin1@mail.com")
+
         Event.objects.create(name="xiaomi5",limit=2000,address='beijing',status=1,start_time='2017-8-10 12:30:00')
         self.login_user = {'username':'admin','password':'admin123456'}
 
     def test_event_manage_success(self):
         #测试发布会:xiaomi5
-        response = self.client.post('/login_action/',data=self.login_user)
+        login_user = {'username': 'admin', 'password': 'admin123456'}
+        Event.objects.create(name="xiaomi5", limit=2000, address='beijing', status=1, start_time='2017-8-10 12:30:00')
+        response = self.client.post('/login_action/',data=login_user)
+        self.assertEqual(response.status_code, 302)
         response = self.client.post('/event_mange/')
         self.assertEqual(response.status_code,200)
         self.assertIn(b"xiaomi5",response.content)
@@ -77,7 +87,10 @@ class EventManageTest(TestCase):
 
     def test_event_manage_search_success(self):
         #测试发布会搜索
-        response = self.client.post('/login_action/',data=self.login_user)
+        login_user = {'username': 'admin', 'password': 'admin123456'}
+        Event.objects.create(name="xiaomi5", limit=2000, address='beijing', status=1, start_time='2017-8-10 12:30:00')
+        response = self.client.post('/login_action/',data=login_user)
+        self.assertEqual(response.status_code, 302)
         response = self.client.post('/search_mange/',{"name":"xiaomi5"})
         self.assertEqual(response.status_code,200)
         self.assertIn(b"xiaomi5",response.content)
---
+++ guest/sign/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from sign import views_if
+
+urlpatterns = [
+    # sign system inteface:
+    # ex:/api/add_event/
+    #url('add_event/',view_if.add_event,name='add_event'),
+    # ex:/api/add_guest/
+    #url('add_guest/',view_if.add_guest,name='add_guest'),
+    # ex:/api/get_event_list/
+    #url('get_event_list/',view_if.get_event_list,name='get_event_list'),
+    # ex:/api/get_guest_list/
+    #url('get_guest_list/',view_if.get_guest_list,name='get_guest_list'),
+    # ex:/api/user_sign/
+    #url('user_sign/',view_if.user_sign,name='user_sign'),
+]
